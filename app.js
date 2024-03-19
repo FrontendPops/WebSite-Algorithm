f710d574beef59d9c741166a92379031be491b29
Хорев Павел Александрович <khorev05@list.ru>
2024-03-19 23:09:00 +0700

    создание области с возможностью ставить преграды

--- app.js
+++ app.js
@@ -1,31 +1,43 @@
+
+
 document.getElementById("matrixSize").addEventListener("input", function displayField() {
-    const matrixSize = document.getElementById("matrixSize").value;
-    const matrix = Array.from({ length: matrixSize }, () => Array.from({ length: matrixSize }, () => 0));
-    
-    const matrixContainer = document.getElementById('matrixContainer');
-    matrixContainer.innerHTML = ''; // Clear previous content
-    
-    // Loop through the matrix and create cells
-    for (let i = 0; i < matrix.length; i++) {
-        const rowDiv = document.createElement('div');
-        rowDiv.className = 'row clearfix'; // For clearfix
-        
-        for (let j = 0; j < matrix[i].length; j++) {
+    const matrixSize = parseInt(document.getElementById("matrixSize").value);
+    const containerWidth = 500;
+    const containerHeight = 500;
+    const sizeSquare = containerWidth / matrixSize;
+
+    const matrixContainer = document.getElementById('container');
+    matrixContainer.innerHTML = '';
+
+    for (let i = 0; i < matrixSize; i++) {
+        for (let j = 0; j < matrixSize; j++) {
             const cellDiv = document.createElement('div');
             cellDiv.className = 'cell';
+            cellDiv.style.width = sizeSquare + 'px';
+            cellDiv.style.height = sizeSquare + 'px';
+            cellDiv.style.backgroundColor = '#FFFFFF';
+            cellDiv.style.margin = '0';
+            cellDiv.style.position = 'absolute';
+
+            const leftPos = sizeSquare * j;
+            const topPos = sizeSquare * i;
+
+            cellDiv.style.left = leftPos + 'px';
+            cellDiv.style.top = topPos + 'px';
             
-            if (matrix[i][j] === 0) {
-                cellDiv.style.backgroundColor = '#fff'; // Set empty cell color
-            } else {
-                cellDiv.style.backgroundColor = '#000'; // Set filled cell color
-            }
-            
-            rowDiv.appendChild(cellDiv);
+            cellDiv.addEventListener('click', function() {
+                if (cellDiv.style.backgroundColor === 'rgb(128, 128, 128)') {
+                    cellDiv.style.backgroundColor = '#FFFFFF';
+                } else {
+                    cellDiv.style.backgroundColor = '#808080';
+                }
+            });
+
+            matrixContainer.appendChild(cellDiv);
         }
-        
-        matrixContainer.appendChild(rowDiv);
     }
+    matrixContainer.style.width = containerWidth + 'px';
+    matrixContainer.style.height = containerHeight + 'px';
 
+    
 });
-
-
