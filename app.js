100c84690006279ef367048efc4566a50805eeff
gautamaPopados <egornexus10@gmail.com>
2024-03-21 22:29:30 +0700

    simple clustering done

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-let points = [];
+let pointList = [];
 
 canvas.addEventListener('mousedown', (event) => {
 	const rect = canvas.getBoundingClientRect();
 	const xVal = event.clientX - rect.left;
 	const yVal = event.clientY - rect.top;
 
-	points.push({ x: xVal, y: yVal });
+	pointList.push({ x: xVal, y: yVal });
   console.log(event.clientX, event.clientY);
 	drawPoints();
 });
@@ -16,98 +16,136 @@ canvas.addEventListener('mousedown', (event) => {
 function drawPoints() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	ctx.fillStyle = '#0';
-	for (let point of points) {
+	
+	for (let point of pointList) {
+    ctx.fillStyle = '#0';
 		ctx.fillRect(point.x, point.y, 10, 10);
 	}
 }
 
 function distance(point1, point2) {
     return Math.sqrt(Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2));
-  }
-  
-function initializeClusters(points, k) {
-    let clusters = [];
-    for (let i = 0; i < k; i++) {
-        clusters.push({
-        index: i,
-        points: [points[i]],
-        centroid: { x: points[i].x, y: points[i].y }
-        });
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function compareCentroids(a, b) {
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
     }
-    for (let i = k; i < points.length; i++) {
+  }
+  return true;
+}
+  
+function initializeClusters(pointList, k) {
+    let clusters = [];
+    let centroids = [];
+
+    for (let i = 0; i < k; i++) {
+      while (true) {
+        let checked = true;
+        centroidCandidate = pointList[getRandomInt(pointList.length)];
+        for (let j = 0; j < centroids.length; j++) {
+          if (centroidCandidate.x == centroids[j].x ||centroidCandidate.xy == centroids[j].y) checked = false;
+        }
+        if (checked) break;
+      }
+      centroids.push(centroidCandidate);
+    }
+    for (let i = 0; i < k; i++) {
+
+      let newCluster = {
+        index: i,
+        points: [
+          {x: centroids[i].x,
+          y: centroids[i].y}
+        ],
+        centroid: {
+          x: centroids[i].x,
+          y: centroids[i].y
+        }
+      };
+      console.log("CENTROID " + newCluster.centroid.x + " " + newCluster.centroid.y);
+      clusters.push(newCluster);
+    }
+
+    for (let i = 0; i < pointList.length; i++) {
         let minDistance = Infinity;
         let clusterIndex = -1;
         for (let j = 0; j < k; j++) {
-            let distanceToCluster = distance(points[i], clusters[j].centroid);
+            let distanceToCluster = distance(pointList[i], clusters[j].centroid);
             if (distanceToCluster < minDistance) {
                 minDistance = distanceToCluster;
                 clusterIndex = j;
             }
         }
-        clusters[clusterIndex].points.push(points[i]);
+        clusters[clusterIndex].points.push(pointList[i]);
     }
-    console.log(clusters);
     return clusters;
 }
   
-  function updateClusters(clusters, points) {
-    const newClusters = clusters.map(cluster => {
-      const sumX = cluster.points.reduce((acc, point) => acc + point.x, 0);
-      const sumY = cluster.points.reduce((acc, point) => acc + point.y, 0);
-      const centroid = {
-        x: sumX / cluster.points.length,
-        y: sumY / cluster.points.length
-      };
-      return { ...cluster, centroid };
-    });
-    
-;
+function updateClusters(clusters, pointList) {
 
-    for (let point of points) {
-      let minDistance = Infinity;
-      let closestClusterIndex = -1;
-
-      for (let m = 0; m < newClusters.length; m++) {
-        const distanceToCluster = distance(point, newClusters[m].centroid);
-
-        if (distanceToCluster < minDistance) {
-          minDistance = distanceToCluster;
-          closestClusterIndex = m;
-        }
-      }
-        
-        newClusters[closestClusterIndex].points.push(point);
-      }
-    
-    console.log(newClusters);
-    return newClusters;
-  }
+  const newClusters = clusters.map(cluster => ({
+    index: cluster.index,
+    points: [],
+    centroid:  {
+      x: cluster.points.reduce((acc, point) => acc + point.x, 0) / cluster.points.length,
+      y: cluster.points.reduce((acc, point) => acc + point.y, 0) / cluster.points.length
+    }
+  }));
   
-  function drawClusters(clusters) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let cluster of clusters) {
-      ctx.fillStyle = `hsl(${cluster.index * 360 / clusters.length}, 100%, 50%)`;
-      for (let point of cluster.points) {
-        ctx.fillRect(point.x, point.y, 10, 10);
+
+  for (let point of pointList) {
+    let minDistance = Infinity;
+    let closestClusterIndex = -1;
+
+    for (let m = 0; m < newClusters.length; m++) {
+      const distanceToCluster = distance(point, newClusters[m].centroid);
+
+      if (distanceToCluster < minDistance) {
+        minDistance = distanceToCluster;
+        closestClusterIndex = m;
       }
     }
+      
+      newClusters[closestClusterIndex].points.push(point);
+    }
+  
+  return newClusters;
+}
+  
+function drawClusters(clusters) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let cluster of clusters) {
+    ctx.fillStyle = `hsl(${cluster.index * 360 / clusters.length}, 100%, 50%)`;
+    for (let point of cluster.points) {
+      ctx.fillRect(point.x, point.y, 10, 10);
+    }
+
   }
+}
   
-  const k = 5;
-  
-  const startButton = document.getElementById('start-button');
-  startButton.addEventListener('click', () => {
-    console.log("Starting...");
-    let clusters = initializeClusters(points, k);
+let input = document.querySelector('input');
+const startButton = document.getElementById('start-button');
+
+startButton.addEventListener('click', () => {
+  console.log("Starting..."); 
+  let k = input.value;
+  if (pointList.length >= k) {
+    let clusters = initializeClusters(pointList, k);
     let iteration = 0;
     let isConverged = false;
-
-    while (iteration < 100) {
-      console.log(iteration);
-      clusters = updateClusters(clusters, points);
+  
+    while (iteration < 50) {
+      clusters = updateClusters(clusters, pointList);
       iteration++;
     }
     drawClusters(clusters);
-  });
+  }
+  
+});
   
