71a50ebd3c19d837977d200cc721702300d2f3d3
Daniil <kokolovdaniil@gmail.com>
2024-04-14 10:04:08 +0300

    change

--- construction.js
+++ construction.js
@@ -1,95 +1,95 @@
-import { colors, canvas, context, startBtn, clearBtn, createPoints } from './const.js' 
-import { createPopulation, evolvePopulation, getFittestIndividual, findBestPath, shuffleArray } from './genetic.js' 
- 
- 
-let points = []; 
-let bestPath = []; 
-let allowAddPoints = true; 
- 
-// Кнопка для установки вершин 
-createPoints.addEventListener('click', () => { 
-    allowAddPoints = true; 
-    createPointsCanvas(); 
-}); 
- 
-// Рисуем на холсте "canvas" - вершины, в дальнейшем для отображения пути 
-function createPointsCanvas() { 
-    canvas.addEventListener('click', function (event) { 
-        if (allowAddPoints) { 
-            const rect = canvas.getBoundingClientRect(); 
-            const x = event.clientX - rect.left; 
-            const y = event.clientY - rect.top; 
-            points.push({ x, y }); 
-            drawPoints(); 
-        } 
-    }); 
-} 
- 
-// Кнопка для запуска алгоритма 
-startBtn.addEventListener('click', async function () { 
-    // Мешаем вершины местами 
-    shuffleArray(points); 
- 
-    // Выставляем значения для популяции, процента мутации и количества поколений 
-    const populationSize = 1000; 
-    const mutationRate = 0.05; 
-    const generations = 50; 
-    console.log(generations);
+import { colors, canvas, context, startBtn, clearBtn, createPoints } from './const.js'
+import { findBestPath, shuffleArray } from './genetic.js'
 
-    // Получаем все пути на каждой итерации алгоритма
-    const allPaths = findBestPath(points, populationSize, mutationRate, generations);
 
-    // Отображаем каждый путь на холсте Canvas
-    for (let i = 0; i < allPaths.length; i++) {
-        drawPath(allPaths[i]);
-        console.log(allPaths[i])
-        await sleep(500); // Добавляем задержку для визуализации
-    }
+// Объявление временных переменных, массивов для работы с ними
+function initializeVariables() {
+    let points = [];
+    let allowAddPoints = true;
 
-    // // Выбираем самый оптимальный путь из последней итерации и отображаем его
-    // bestPath = allPaths[allPaths.length - 1];
-    // drawPath(bestPath);
-
-    allowAddPoints = false; // Установка false, после запуска алгоритма нельзя добавлять вершины, пока не нажмется кнопка "Установить вершины" 
-});
- 
- 
-// Кнопка очистки холста "canvas" 
-clearBtn.addEventListener('click', () => { 
-    context.clearRect(0, 0, canvas.width, canvas.height); 
-    points.splice(0, points.length); 
-}); 
- 
-// Рисование вершин на холсте "canvas" 
-function drawPoints() { 
-    context.clearRect(0, 0, canvas.width, canvas.height); 
-    points.forEach(point => { 
-        context.beginPath(); 
-        context.arc(point.x, point.y, 10, 0, Math.PI * 2); 
-        context.fillStyle = colors.orange; 
-        context.fill(); 
-    }); 
-} 
- 
-// Рисование пути по вершинам 
-function drawPath(path) { 
-    drawPoints(); 
-    context.beginPath(); 
-    context.moveTo(points[path[0]].x, points[path[0]].y); 
-    for (let i = 1; i < path.length; i++) { 
-        context.lineTo(points[path[i]].x, points[path[i]].y); 
-    } 
-    context.lineTo(points[path[0]].x, points[path[0]].y); 
-    // let gradient = context.createLinearGradient(0, 0, canvas.width, canvas.height); 
-    // gradient.addColorStop(0, "rgba(255, 165, 0, 1)"); 
-    // gradient.addColorStop(1, "rgba(0, 255, 0, 1)"); 
-    context.lineWidth = 1; 
-    context.strokeStyle = "grey"; 
-    context.stroke(); 
-} 
- 
-// Задержка для показа прохода по вершинам 
-function sleep(ms) { 
-    return new Promise(resolve => setTimeout(resolve, ms)); 
+    return { points, allowAddPoints };
 }
 
+let { points, allowAddPoints } = initializeVariables();
+
+// Кнопка для установки вершин  
+createPoints.addEventListener('click', () => {
+    allowAddPoints = true;
+
+    // Удаляем старый обработчик события click, чтобы они не оставались активными и повторялись
+    canvas.removeEventListener('click', createPointsCanvas);
+
+    canvas.addEventListener('click', createPointsCanvas);
+});
+
+// Рисуем на холсте "canvas" - вершины, в дальнейшем для отображения пути  
+function createPointsCanvas(event) {
+    if (allowAddPoints) {
+        const rect = canvas.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        points.push({ x, y });
+        drawPoints();
+    }
+}
+
+// Кнопка для запуска алгоритма  
+startBtn.addEventListener('click', async function () {
+    // Мешаем вершины местами  
+    console.log(points.length)
+    shuffleArray(points);
+
+    // Выставляем значения для популяции, процента мутации и количества поколений  
+    const populationSize = 5000;
+    const mutationRate = 0.7;
+    const generations = points.length * 10;
+    console.log(generations);
+
+    // Получаем все пути на каждой итерации алгоритма 
+    const allPaths = findBestPath(points, populationSize, mutationRate, generations);
+
+    // Отображаем каждый путь на холсте Canvas 
+    for (let i = 0; i < allPaths.length; i++) {
+        drawPath(allPaths[i]);
+        await sleep(200); // Добавляем задержку для визуализации 
+    }
+
+    allowAddPoints = false; // Установка false, после запуска алгоритма нельзя добавлять вершины, пока не нажмется кнопка "Установить вершины"  
+});
+
+
+// Кнопка очистки холста "canvas"  
+clearBtn.addEventListener('click', () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    points.splice(0, points.length);
+});
+
+// Рисование вершин на холсте "canvas"  
+function drawPoints() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    points.forEach(point => {
+        context.beginPath();
+        context.arc(point.x, point.y, 10, 0, Math.PI * 2);
+        context.fillStyle = colors.orange;
+        context.fill();
+    });
+}
+
+// Рисование пути по вершинам  
+function drawPath(path) {
+    drawPoints();
+    context.beginPath();
+    context.moveTo(points[path[0]].x, points[path[0]].y);
+    for (let i = 1; i < path.length; i++) {
+        context.lineTo(points[path[i]].x, points[path[i]].y);
+    }
+    context.lineTo(points[path[0]].x, points[path[0]].y);
+    context.lineWidth = 1;
+    context.strokeStyle = "grey";
+    context.stroke();
+}
+
+// Задержка для показа прохода по вершинам  
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
--- genetic.js
+++ genetic.js
@@ -1,99 +1,91 @@
-
 export function createPopulation(populationSize, vertex) {
     let population = [];
     for (let i = 0; i < populationSize; i++) {
         const path = Array.from({ length: vertex }, (_, index) => index);
-        shuffleArray(path); // произвольно делаем начальный путь 
-        population.push({ path, fitness: 0 }); // path - путь, fitness: 0 выставляем изначально для каждого уровень приспособленности 
+        shuffleArray(path); // произвольно делаем начальный путь  
+        population.push({ path, fitness: 0 }); // path - путь, fitness: 0 выставляем изначально для каждого уровень приспособленности  
     }
     return population;
 }
 
 export function evolvePopulation(population, tournamentSize, mutationRate, points) {
     const newPopulation = [];
-    
+
     while (newPopulation.length < population.length) {
-        // Выбираем случайные индивидов для участия в турнире
+        // Выбираем случайные индивидов для участия в турнире 
         const tournamentParticipants = [];
         for (let i = 0; i < tournamentSize; i++) {
             const randomIndex = Math.floor(Math.random() * population.length);
             tournamentParticipants.push(population[randomIndex]);
         }
-        
-        // Находим победителя турнира (индивида с наивысшей приспособленностью)
+
+        // Находим победителя турнира (индивида с наивысшей приспособленностью) 
         tournamentParticipants.sort((a, b) => b.fitness - a.fitness);
         const parentFirst = tournamentParticipants[0];
-        
-        // Выбираем второго родителя таким же образом
+
+        // Выбираем второго родителя таким же образом 
         let parentSecond;
         do {
             const randomIndex = Math.floor(Math.random() * population.length);
             parentSecond = population[randomIndex];
         } while (parentSecond === parentFirst);
-        
-        // Выполняем кроссовер
+
+        // Выполняем кроссовер 
         const crossoverPoint = Math.floor(Math.random() * parentFirst.path.length);
-        const [childPathFirst, childPathSecond] = crossover(parentFirst.path, parentSecond.path, crossoverPoint);
-        
-        // Применяем мутацию для обоих потомков
-        mutate(childPathFirst, mutationRate);
-        mutate(childPathSecond, mutationRate);
-        
-        // Убедимся, что в каждом потомке содержатся все вершины
+        const [childPathFirst, childPathSecond] = crossover(parentFirst.path, parentSecond.path);
+
+        // Применяем мутацию для обоих потомков 
+        mutate(childPathFirst, mutationRate, points);
+        mutate(childPathSecond, mutationRate, points);
+
+        // Убедимся, что в каждом потомке содержатся все вершины 
         const completeChildPathFirst = makeCompletePath(childPathFirst, points.length);
         const completeChildPathSecond = makeCompletePath(childPathSecond, points.length);
-        
-        // Применяем оптимизацию 2-opt
-        // const optimizedPathFirst = twoOpt(completeChildPathFirst, points);
-        // const optimizedPathSecond = twoOpt(completeChildPathSecond, points);
-        
-        // Обновляем приспособленность для потомков на основе длины их путей
-        const childFitnessFirst = 1 / calculateFitness(completeChildPathFirst, points); // Обратная длина пути
-        const childFitnessSecond = 1 / calculateFitness(completeChildPathSecond, points); // Обратная длина пути
-        
+
+        // Обновляем приспособленность для потомков на основе длины их путей 
+        const childFitnessFirst = calculateFitness(completeChildPathFirst, points); // Обратная длина пути 
+        const childFitnessSecond = calculateFitness(completeChildPathSecond, points); // Обратная длина пути 
+
         newPopulation.push({ path: completeChildPathFirst, fitness: childFitnessFirst });
         newPopulation.push({ path: completeChildPathSecond, fitness: childFitnessSecond });
     }
-    
+
     return newPopulation;
 }
 
-function makeCompletePath(path, vertexCount) {
-    const missingVertices = Array.from({ length: vertexCount }, (_, index) => index).filter(vertex => !path.includes(vertex));
+function makeCompletePath(path, points) {
+    const missPoints = Array.from({ length: points }, (_, index) => index).filter(point => !path.includes(point));
     const completePath = [...path];
-    for (const vertex of missingVertices) {
-        completePath.push(vertex);
+    for (const point of missPoints) {
+        completePath.push(point);
     }
     return completePath;
 }
 
-function crossover(parentFirst, parentSecond, crossoverPoint) {
-    const childFirst = [];
-    const childSecond = [];
+function crossover(parentFirst, parentSecond) {
 
-    // Копируем часть родителей до точки перекрещивания
-    childFirst.push(...parentFirst.slice(0, crossoverPoint));
-    childSecond.push(...parentSecond.slice(0, crossoverPoint));
-
-    // Заполняем оставшуюся часть потомков оставшимися вершинами из других родителей
-    let indexFirst = crossoverPoint;
-    let indexSecond = crossoverPoint;
-
+    const crossoverPoint1 = Math.floor(Math.random() * parentFirst.length);
+    const crossoverPoint2 = Math.floor(Math.random() * parentFirst.length);
+    
+    // Смотрим, что crossoverPoint1 меньше crossoverPoint2
+    const start = Math.min(crossoverPoint1, crossoverPoint2);
+    const end = Math.max(crossoverPoint1, crossoverPoint2);
+    
+    // Копируем сегмент родительского пути в потомков
+    const child1 = parentFirst.slice(start, end + 1);
+    const child2 = parentSecond.slice(start, end + 1);
+    
     for (let i = 0; i < parentFirst.length; i++) {
-        if (!childFirst.includes(parentSecond[i])) {
-            childFirst.push(parentSecond[i]);
+        if (!child1.includes(parentSecond[(end + 1 + i) % parentFirst.length])) {
+            child1.push(parentSecond[(end + 1 + i) % parentFirst.length]);
         }
-
-        if (!childSecond.includes(parentFirst[i])) {
-            childSecond.push(parentFirst[i]);
+        
+        if (!child2.includes(parentFirst[(end + 1 + i) % parentFirst.length])) {
+            child2.push(parentFirst[(end + 1 + i) % parentFirst.length]);
         }
-
-        // Переходим к следующей вершине в родителях
-        indexFirst = (indexFirst + 1) % parentFirst.length;
-        indexSecond = (indexSecond + 1) % parentFirst.length;
     }
-
-    return [childFirst, childSecond];
+    
+    return [child1, child2];
 }
 
 
@@ -113,45 +105,24 @@ export function getFittestIndividual(population, points) {
     return population.reduce((prev, current) => calculatePathDistance(prev.path, points) < calculatePathDistance(current.path, points) ? prev : current);
 }
 
-function twoOptSwap(path, i, k) {
-    const newPath = path.slice(0, i);
-    newPath.push(...path.slice(i, k + 1).reverse());
-    newPath.push(...path.slice(k + 1));
-    return newPath;
-}
-
-// export function findBestPath(points, populationSize, mutationRate, generations) {
-//     let population = createPopulation(populationSize, points.length);
-//     let allPaths = [];
-//     let fittestIndividual = 0;
-//     for (let generation = 0; generation < generations; generation++) {
-//         population = evolvePopulation(population, mutationRate, points);
-//         fittestIndividual = getFittestIndividual(population, points);
-//         allPaths.push(fittestIndividual.path)
-//         allPaths.push(twoOpt(fittestIndividual.path, points));
-//     }
-//     return allPaths;
-// }
-
 export function findBestPath(points, populationSize, mutationRate, generations) {
     let population = createPopulation(populationSize, points.length);
-    let uniquePaths = new Set(); // Используем Set для хранения уникальных путей
+    let uniquePaths = new Set(); // Используем Set для хранения уникальных путей 
     let fittestIndividual = 0;
 
     for (let generation = 0; generation < generations; generation++) {
-        population = evolvePopulation(population, 5, mutationRate, points);
+        population = evolvePopulation(population, 15, mutationRate, points);
         fittestIndividual = getFittestIndividual(population, points);
 
-        // Добавляем только уникальные пути в массив
+        // Добавляем только уникальные пути в массив 
         uniquePaths.add(JSON.stringify(fittestIndividual.path));
-        uniquePaths.add(JSON.stringify(twoOpt(fittestIndividual.path, points)));
     }
 
-    // Преобразуем уникальные пути из Set обратно в массив и возвращаем
+    // Преобразуем уникальные пути из Set обратно в массив и возвращаем 
     return Array.from(uniquePaths).map(path => JSON.parse(path));
 }
 
-// Рассчитывание длину пути от одной вершины до другой 
+// Рассчитывание длину пути от одной вершины до другой  
 export function calculatePathDistance(path, points) {
     let distance = 0;
     for (let i = 0; i < path.length - 1; i++) {
@@ -162,7 +133,7 @@ export function calculatePathDistance(path, points) {
 }
 
 
-// Перемешка для случайного порядка - алгоритм (Fisher–Yates shuffle) 
+// Перемешка для случайного порядка - алгоритм (Fisher–Yates shuffle)  
 export function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -172,24 +143,5 @@ export function shuffleArray(array) {
 
 export function calculateFitness(path, points) {
     const distance = calculatePathDistance(path, points);
-    return 1 / distance; // Обратная длина пути, чем меньше длина, тем выше приспособленность
-}
-
-function twoOpt(path, points) {
-    let improved = true;
-    while (improved) {
-        improved = false;
-        for (let i = 0; i < path.length - 2; i++) {
-            for (let k = i + 2; k < path.length; k++) {
-                const newPath = twoOptSwap(path, i, k);
-                const newPathFitness = calculateFitness(newPath, points);
-                const currentPathFitness = calculateFitness(path, points);
-                if (newPathFitness > currentPathFitness) { // Оцениваем приспособленность
-                    path = newPath;
-                    improved = true;
-                }
-            }
-        }
-    }
-    return path;
+    return 1 / distance; // Обратная длина пути, чем меньше длина, тем выше приспособленность 
 }
