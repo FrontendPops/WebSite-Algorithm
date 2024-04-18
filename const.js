8fa08bc26573387a13351e39cfe3f673e51489f1
Daniil <kokolovdaniil@gmail.com>
2024-04-18 14:41:32 +0300

    change

--- const.js
+++
@@ -1,9 +0,0 @@
-export const canvas = document.getElementById('canvas');
-export const context = canvas.getContext('2d');
-export const startBtn = document.getElementById('startBtn');
-export const clearBtn = document.getElementById('clearBtn');
-export const createPoints = document.getElementById('createPoints');
-
-export const colors = {
-    orange: '#FF8000'
-};
