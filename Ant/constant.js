388957c679e3cb5d6ecb92eeec95aad39605fb7c
Хорев Павел Александрович <khorev05@list.ru>
2024-04-19 19:04:34 +0700

    изменение

---
+++ Ant/constant.js
@@ -0,0 +1,12 @@
+export const colors = {
+    pinkColor: '#FF1493',
+    redColor: '#FF0000',
+    blackColor: '#000000'
+};
+
+export const numberPI = Math.PI;
+
+export const buttonClick = {
+    searchWay: false,
+    cleanWay: false
+}
